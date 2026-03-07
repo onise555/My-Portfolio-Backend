0856c68a66c77b204d2ca53eb32c06825c2b2f3b
Just coder <138765040+Writer-Of-Something-0000@users.noreply.github.com>
2026-03-07 13:07:43 +0400

    update

--- Portfolio.Asp/Portfolio.Asp/Models/Users/Profile.cs
+++ Portfolio.Asp/Portfolio.Asp/Models/Users/Profile.cs
@@ -1,4 +1,5 @@
-﻿using Portfolio.Asp.Models.SocialLinks;
+﻿using Portfolio.Asp.Models.languages;
+using Portfolio.Asp.Models.SocialLinks;
 
 namespace Portfolio.Asp.Models.User
 {
@@ -14,8 +15,9 @@ namespace Portfolio.Asp.Models.User
 
 
         // One-To-Many კავშირი 
-        public List<SocialLink> links { get; set; }
+        public List<SocialLink> links { get; set; } = new List<SocialLink>();
 
+        public List<language> languages { get; set; } = new List<language>();
 
 
 
