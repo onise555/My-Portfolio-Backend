b0f062a0fe62ed7f86c27c9f08793ed7553a24ca
Just coder <138765040+Writer-Of-Something-0000@users.noreply.github.com>
2026-03-07 22:26:48 +0400

    jandabaonises

---
+++ Portfolio.Asp/Portfolio.Asp/requests/User/CreateUserRequest.cs
@@ -0,0 +1,10 @@
+namespace Portfolio.Asp.requests.User;
+
+public class CreateUserRequest
+{
+    public string FullName { get; set; }
+
+    public IFormFile ProfileImage { get; set; }
+    
+    public IFormFile ProfileVideo { get; set; }
+}
