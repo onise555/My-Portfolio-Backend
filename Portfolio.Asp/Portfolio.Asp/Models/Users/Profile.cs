46300c659e6707688f221407e103c7dc7d9935da
Just coder <138765040+Writer-Of-Something-0000@users.noreply.github.com>
2026-03-07 13:45:08 +0400

    updater

--- Portfolio.Asp/Portfolio.Asp/Models/Users/Profile.cs
+++ Portfolio.Asp/Portfolio.Asp/Models/Users/Profile.cs
@@ -1,7 +1,7 @@
 
-﻿using Portfolio.Asp.Models.Academies;
+using Portfolio.Asp.Models.Academies;
 
-﻿using Portfolio.Asp.Models.Contacts;
+using Portfolio.Asp.Models.Contacts;
 
 using Portfolio.Asp.Models.languages;
 using Portfolio.Asp.Models.SocialLinks;
