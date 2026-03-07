5edc23ed114ad40e07993f74aa92d8a62f2f89c6
Onise <tsotskhalashvili558@gmail.com>
2026-03-07 12:22:27 +0400

    ubgrade models

---
+++ Portfolio.Asp/Portfolio.Asp/Models/User/Profile.cs
@@ -0,0 +1,15 @@
+﻿namespace Portfolio.Asp.Models.User
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string About { get; set; }
+
+
+        // One-To-One კავშირი  
+        public int UserId { get; set; }
+        public User User { get; set; }  
+
+
+
+    }
---
+++ Portfolio.Asp/Portfolio.Asp/Models/User/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Portfolio.Asp.Models.User
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public string ProfileImage { get; set; }
+
+        public string ProfileVideo { get; set; }
+
+        public Profile Profile {  get; set; }
+
+
+    }
+}
