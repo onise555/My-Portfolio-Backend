72b6a0245306ac4b97d7850cec3e3691cad6534a
Onise <tsotskhalashvili558@gmail.com>
2026-03-07 16:22:56 +0400

    Upgrade

--- Portfolio.Asp/Portfolio.Asp/Models/Projects/ProjectCategories.cs
+++ Portfolio.Asp/Portfolio.Asp/Models/Projects/ProjectCategories.cs
@@ -3,9 +3,10 @@
     public class ProjectCategories
     {
         public int Id { get; set; }
+        public string Name { get; set; }
 
-        public string Name { get; set; }    
 
+        // One-To-Many კავშირი
         public List<ProjectSubCategories> ProjectSubCategories { get; set; } = new List<ProjectSubCategories>();    
     }
 }
