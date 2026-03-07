85625b22e5e8db3520dbb5fa76d152691302f6fb
Just coder <138765040+Writer-Of-Something-0000@users.noreply.github.com>
2026-03-07 13:00:15 +0400

    davamate enum

--- Portfolio.Asp/Portfolio.Asp/Models/languages/language.cs
+++ Portfolio.Asp/Portfolio.Asp/Models/languages/language.cs
@@ -1,6 +1,17 @@
+using Portfolio.Asp.Enum;
+using Portfolio.Asp.Models.User;
+
 namespace Portfolio.Asp.Models.languages;
 
 public class language
 {
-    public 
+    public int id { get; set; }
+    public string Name { get; set; }
+    public LanguageLevel level { get; set; }
+    
+    
+    // Many-To-One კავშირი  
+    
+    public int ProfileId { get; set; }
+    public Profile Profile { get; set; }
 }
