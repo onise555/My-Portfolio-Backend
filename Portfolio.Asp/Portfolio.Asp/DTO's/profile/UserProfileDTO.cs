4dc1a12bf881f0f9cfa9b6819f5a1e04ec99dc88
Just coder <138765040+Writer-Of-Something-0000@users.noreply.github.com>
2026-03-14 14:37:02 +0400

    addprofile
    add profile classes tos and requests

---
+++ Portfolio.Asp/Portfolio.Asp/DTO's/profile/UserProfileDTO.cs
@@ -0,0 +1,7 @@
+namespace Portfolio.Asp.DTO_s.profile;
+
+public class UserProfileDTO
+{
+    public int id { get; set; }
+    public string About { get; set; }
+}
---
+++ Portfolio.Asp/Portfolio.Asp/requests/user_profile/UpdateProfileRequest.cs
@@ -0,0 +1,9 @@
+namespace Portfolio.Asp.requests.user_about;
+
+public class UpdateProfileRequest
+{
+
+    public int id { get; set; }
+    public string About { get; set; }
+
+}
