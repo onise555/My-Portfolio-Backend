f170e44e679df870552f5bd3fdcf67cad3f1d083
Onise <tsotskhalashvili558@gmail.com>
2026-03-14 20:05:40 +0400

    Add

--- Portfolio.Asp/Portfolio.Asp/Services/ProfilleSer/IProfileService.cs
+++ Portfolio.Asp/Portfolio.Asp/Services/ProfilleSer/IProfileService.cs
@@ -4,8 +4,8 @@ using Portfolio.Asp.requests.Profile;
 public interface IProfileService
 {
     Task Create(CreateProfilerequest request);
-    Task Update(UpdateProfilerequest request);
+    Task Update(int UserId, UpdateProfilerequest request);
     Task Delete(int id);
-    Task<UserProfileDTO?> GetById(int id);       
+    
     Task<UserProfileDTO?> GetByUserId(int userId); 
 }
