034988da5152d882e22bc8a278de203854bb30d0
Onise <tsotskhalashvili558@gmail.com>
2026-03-14 21:19:43 +0400

    Update

--- Portfolio.Asp/Portfolio.Asp/DTOS/Profile/UserProfileDTO.cs
+++ Portfolio.Asp/Portfolio.Asp/DTOS/Profile/UserProfileDTO.cs
@@ -2,7 +2,7 @@ namespace Portfolio.Asp.DTO_s.profile;
 
 public class UserProfileDTO
 {
+    public int UserId { get; set; }
     public int Id { get; set; }
     public string About { get; set; }
-    public int UserId { get; set; } 
 }
--- Portfolio.Asp/Portfolio.Asp/requests/Profile/UpdateProfilerequest.cs
+++ Portfolio.Asp/Portfolio.Asp/requests/Profile/UpdateProfilerequest.cs
@@ -2,8 +2,8 @@ namespace Portfolio.Asp.requests.Profile;
 
 public class UpdateProfilerequest
 {
-    public string About { get; set; }
     public int UserId { get; set; }
 
-    
+    public string About { get; set; }
+
 }
